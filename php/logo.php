7d973297178d2fee2eb5283cec4ac802c14cc616
Xavier Dupre <dupre.xavier@courrier.uqam.ca>
2019-12-06 17:44:57 -0500

    Logo SVG

---
+++ php/logo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<title>Connexion</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+<link rel="stylesheet" type="text/css" href="../css/index.css" />
+</head>
+<body>
+<?php
+require 'lib/header.php';
+require 'lib/nav.php';
+?>
+<h2>Futur Logo du Syndicat</h2>
+<svg id="nouveau" width="400" height="180">
+<rect x="50" y="20" rx="20" ry="20" width="150" height="150"
+fill="lightblue" stroke="black" stroke-width="5"/>
+<text fill="black" font-size="24" font-family="Verdana" x="75" y="100" >Syndicat</text>
+</svg>
+</body>
+<?php require 'lib/footer.php'; ?>
+</html>
