bcc952965a02ae7df1bf85d5fb74004620e499c5
Xavier1520 <dupre.xavier@hotmail.fr>
2019-12-09 13:48:47 -0500

    Solution BD

--- php/formulaire3.php
+++ php/formulaire3.php
@@ -1,5 +1,13 @@
 <?php
-require 'config.php';
+require 'lib/Medoo.php';
+use Medoo\Medoo;
+$database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'bd_ej591065',
+        'server' => 'zeta2.labunix.uqam.ca',
+        'username' => 'ej591065',
+        'password' => 'yxZqOLSX'
+]);
 class Formulaire {
 	public function afficherForm() {
 		global $database;
--- php/resultat1.php
+++ php/resultat1.php
@@ -10,7 +10,15 @@
 <?php
 require 'lib/header.php';
 require 'lib/nav.php';
-require 'config.php';
+require 'lib/Medoo.php';
+use Medoo\Medoo;
+$database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'bd_ej591065',
+        'server' => 'zeta2.labunix.uqam.ca',
+        'username' => 'ej591065',
+        'password' => 'yxZqOLSX'
+]);
 if(isset($_REQUEST["soumettre"]))
 {
 	$nom=$_REQUEST["nom"];
