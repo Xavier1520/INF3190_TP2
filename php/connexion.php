ec55dbacbda33a71aaa8076294510ab6c740d605
Xavier Dupre <dupre.xavier@courrier.uqam.ca>
2019-12-10 19:07:21 -0500

    Suite correction

--- php/connexion.php
+++ php/connexion.php
@@ -4,10 +4,11 @@
 <meta charset="utf-8" />
 <title>Syndicat des travailleurs</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-<link rel="stylesheet" type="text/css" href="../jstree/dist/themes/default/style.min.css" />
 <link rel="stylesheet" type="text/css" href="../css/index.css" />
 <link rel="stylesheet" type="text/css" href="../css/nav.css" />
 <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../jstree/dist/jstree.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../jstree/dist/themes/default/style.min.css" />
 <script type="text/javascript" src="../js/organigramme.js"></script>
 </head>
 <body>
@@ -27,10 +28,8 @@ Mot de passe: <input type="password" pattern="^[A-Za-z0-9?*-+ ]{8,12}$" required
 </div>
 </form>
 <h2>Organigramme des membres du syndicat</h2>
-<div class="submitreset">
-<button id="organigramme">Afficher</button>
-<button id="resetorg">Suprimmer</button>
-</div>
+<button id="organigramme" class="btn btn-primary">Afficher</button>
+<button id="resetorg" class="btn btn-danger">Suprimmer</button>
 <div id="afficher"></div>
 </body>
 <?php require 'lib/footer.php'; ?>
--- php/manifestants.php
+++ php/manifestants.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
-		<link rel="stylesheet" style="text/css" href="../css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
-		<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
-		<link rel="stylesheet" type="text/css" href="../css/index.css" />
-		<script type="text/javascript" src="../js/manifestants.js"></script>
-		<title>Syndicat des travailleurs</title>
-	</head>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<link rel="stylesheet" style="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/index.css" />
+<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+<script type="text/javascript" src="../js/manifestants.js"></script>
+<title>Syndicat des travailleurs</title>
+</head>
 <?php
 require 'lib/header.php';
 require 'lib/nav.php';
